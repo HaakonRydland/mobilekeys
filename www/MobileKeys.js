df2af2948b9717b25d5460aec9a1924fe28fcbb0
UNIPLUSS\hakon.rydland <hakon.rydland@unipluss.no>
2019-07-15 13:21:11 +0200

    Skrevet JavaScript interface for kontakt med native kode

--- www/MobileKeys.js
+++ www/MobileKeys.js
@@ -1,5 +1,36 @@
+// setup information
 var exec = require('cordova/exec');
 
-exports.coolMethod = function (arg0, success, error) {
-    exec(success, error, 'MobileKeys', 'coolMethod', [arg0]);
+function MobileKeys() {}
+
+MobileKeys.prototype.PluginName = "MobileKeys";
+
+MobileKeys.prototype.pInvoke = function(method, data, callbackOk, callbackError) {
+    console.log('Inside pInvoke');
+
+    if (data == null || data == undefined) {
+        data = [];
+    } else if (!Array.isArray(data)) {
+        data = [data];
+    }
+    exec(callbackOk, callbackError, this.PluginName, method, data);
+}
+
+MobileKeys.install = function() {
+    console.log('Inside install');
+
+    if (!window.plugins) {
+        window.plugins = {};
+    }
+
+    window.plugins.MobileKeys = new MobileKeys();
+
+    return window.plugins.MobileKeys;
+};
+
+cordova.addConstructor(MobileKeys.install);
+
+// Method calls to native code
+MobileKeys.prototype.coolMethod = function (arg0, success, error) {
+    this.pInvoke("coolMethod", data, callbackOk, callbackError);
 };
