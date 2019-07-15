93fb0d95b44a5a19b7554ae1419498172dfad575
UNIPLUSS\hakon.rydland <hakon.rydland@unipluss.no>
2019-07-15 13:03:27 +0200

    Initial commit

---
+++ MobileKeys/src/ios/MobileKeys.m
@@ -0,0 +1,28 @@
+/********* MobileKeys.m Cordova Plugin Implementation *******/
+
+#import <Cordova/CDV.h>
+
+@interface MobileKeys : CDVPlugin {
+  // Member variables go here.
+}
+
+- (void)coolMethod:(CDVInvokedUrlCommand*)command;
+@end
+
+@implementation MobileKeys
+
+- (void)coolMethod:(CDVInvokedUrlCommand*)command
+{
+    CDVPluginResult* pluginResult = nil;
+    NSString* echo = [command.arguments objectAtIndex:0];
+
+    if (echo != nil && [echo length] > 0) {
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:echo];
+    } else {
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
+    }
+
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+}
+
+@end
